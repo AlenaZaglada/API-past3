962038345b8e33a59957831bbfacbe0628c58983
Alena <pluwa@mail.ru>
2023-05-04 15:29:02 +0400

    Test failed

---
+++ src/test/java/TestPostman.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+        import static io.restassured.RestAssured.authentication;
+        import static io.restassured.RestAssured.given;
+        import static org.hamcrest.Matchers.equalTo;
+
+        import org.junit.jupiter.api.Test;
+
+public class TestPostman {
+    @Test
+    void shouldReturnData() {
+// Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("04.05.2023")
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("14.05.2023"));
+    }
+}
